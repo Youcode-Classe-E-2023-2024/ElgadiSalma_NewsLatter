31abf1601fe2f4c722756fabf088d7d623a995f9
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-06 16:46:06 +0100

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -37,11 +37,22 @@
                     <!-- Header Icons -->
                     
                     <!-- Sign In / Register      -->
-                    <a class="flex items-center hover:text-gray-200" href="#">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg>
-                    </a>                 
+                    <div class="flex gap-5">
+
+                        <a class="flex items-center hover:text-gray-200" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                        </a>      
+                        
+                        <a class="flex items-center hover:text-gray-200" href="#">
+                            <form action="{{ route('logout') }}" method="POST">
+                                @csrf
+                                <button type="submit" class="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark border border-gray-100 rounded-xl">Log out</button>
+                            </form>
+                        </a>                  
+                    </div>
+                   
                 </div>
             </nav>
         </section>
