758295413c34de66925c43f973d91b46e371b9c5
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-06 15:19:24 +0100

    Update userController.php

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -3,8 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth; 
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Validation\Rule;
 
 class userController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('login');
+    } 
+    public function login(Request $request)
+    {
+        $email = $request->email;
+        $password = $request->password;
+        $credentials = ['email' => $email , 'password' => $password ];
+        if(Auth::attempt($credentials))
+        {
+            $profile = Auth::user();
+            $request->session()->regenerate(true); 
+            return to_route('dashboard')->with('success', 'Vous êtes bien connecté '.$email." .");
+
+        }else{
+            // shi haja ghalta
+            return back()->withErrors([
+            'email'=> 'Email ou mot de passe incorrect.'
+            ])->onlyInput('email');
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout(); 
+
+        return redirect()->route('login'); 
+        // return view('login');
+
+    }
+
 }
