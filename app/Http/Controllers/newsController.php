b4f03d5e3c38a5fbff3c5397cbc88928361dcc32
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-06 15:19:28 +0100

    Create newsController.php

---
+++ app/Http/Controllers/newsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class newsController extends Controller
+{
+    public function dashboard()
+    {
+        return view('dashboard');
+
+    }
+}
