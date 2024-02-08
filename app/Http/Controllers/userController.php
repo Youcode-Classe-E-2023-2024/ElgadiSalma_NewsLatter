acd9db2e4270d3595888f127b5029d70e7818985
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-08 16:33:30 +0100

    Update userController.php

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -8,6 +8,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 
 class userController extends Controller
 {
@@ -31,11 +34,44 @@ class userController extends Controller
             ])->onlyInput('email');
     }
 
-    public function logout()
+    public function logout(Request $request)
     {
-        Auth::logout(); 
+        Auth::logout();
 
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+ 
         return redirect()->route('login.show'); 
     }
 
+
+    // forgot pass
+
+    public function forgot_show()
+    {
+        return view('forgot-password');
+    }
+
+    public function reset(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|string|confirmed|min:8',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user) use ($request) {
+                $user->forceFill([
+                    'password' => Hash::make($request->password),
+                    'remember_token' => Str::random(60)
+                ])->save();
+            }
+        );
+
+        return $status == Password::PASSWORD_RESET
+            ? redirect()->route('login.show')->with('status', __($status))
+            : back()->withInput($request->only('email'))->withErrors(['email' => __($status)]);
+    }
 }
