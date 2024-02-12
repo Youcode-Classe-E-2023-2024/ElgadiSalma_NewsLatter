2f4f1f1ce6de0ecafc8286fc1d1d3fa6f447b57c
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-12 12:11:21 +0100

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -35,7 +35,7 @@
                     </div>
                     <div>
                         <label class="text-gray-800 font-semibold block my-3 text-md" for="password">Password</label>
-                        <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="password" id="password" placeholder="password" />
+                        <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="password" name="password" id="password" placeholder="password" />
                     </div>
 
                     <div class="flex justify-between pt-5">
