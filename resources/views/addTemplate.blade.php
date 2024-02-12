5a9d8c758dff0936af18b2ef0cbe1988bb57e395
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-12 14:00:37 +0100

    Update addTemplate.blade.php

--- resources/views/addTemplate.blade.php
+++ resources/views/addTemplate.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="heading text-center font-bold text-2xl pt-10 text-yellow-400">Add Template</div>
+<div class="text-4xl text-purple-500 pt-20 text-center font-bold">Add Template</div>
 
 <div class="flex items-center justify-center p-12 ">
 <div class="w-2/3">
@@ -22,17 +22,20 @@
         
         <div class="w-full px-3 sm:w-1/2">
         <div class="mb-5">
-        <label for="Photo" class="mb-3 block text-base font-medium text-[#07074D]" >
-              Photo 
-            </label>
-            <input type="file" name="photo" placeholder="Photo" class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" value="">
-    
-        </div>
+            <label for="titre" class="mb-3 block text-base font-medium text-[#07074D]" >
+                Media 
+              </label>
+            <select name="category" id="category" class="w-full rounded-md title border py-3 px-6  border-gray-300 text-[#6B7280] outline-none" required>
+                <option class="" value="" selected disabled>Media</option>
+                <option value="">zzzz</option>
+            </select>
         </div>
         </div>
 
+        </div>
+
       <div class="mb-5">
-        <label for="guest" class="mb-3 block text-base font-medium text-[#07074D]" >
+        <label class="mb-3 block text-base font-medium text-[#07074D]">
         Description
         </label>
         <input
@@ -44,10 +47,12 @@
         />
         </div>
 
-        <button type="submit" name="submit" class="hover:shadow-form rounded-md bg-purple-500 py-3 px-8 text-center text-base font-semibold w-full border-2 outline-none">
+        <div class="my-10">
+        <button type="submit" name="submit" class="hover:shadow-form rounded-md bg-gray-100 py-3 px-8 text-center text-base font-semibold w-full border-2 outline-none">
           Submit
         </button>
-      </div>
+        </div>
+    </div>
     </form>
   </div>
   </div>
