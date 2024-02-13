a436bc1f9d9f38c393460154cef4319cb7ee67e1
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-13 15:03:28 +0100

    Update 2024_02_06_093055_create_news.php

--- database/migrations/2024_02_06_093055_create_news.php
+++ database/migrations/2024_02_06_093055_create_news.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('news', function (Blueprint $table) {
             $table->id();
-            $table->string('image');
+            $table->string('media');
             $table->string('title');
             $table->string('description');
             $table->timestamps();
