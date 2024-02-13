adc184f16ebfe6dfc7148364793ab6fec70850c4
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-13 10:00:37 +0100

    Update media.blade.php

--- resources/views/media.blade.php
+++ resources/views/media.blade.php
@@ -6,14 +6,14 @@
 
     @auth
     @if(auth()->user()->role === 0)
-    <form class="w-full max-w-lg m-4 pt-5" method="POST" action="" enctype="multipart/form-data">
+    <form class="w-full max-w-lg m-4 pt-5" method="POST" action="{{ route('add.media') }}" enctype="multipart/form-data">
         @csrf
         <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full px-3">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
                     Image
                 </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" name="photo" type="file">
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" name="files[]" type="file">
                 <p class="text-gray-600 text-xs italic">Some tips - as long as needed</p>
             </div>
         </div>
@@ -32,25 +32,26 @@
     <div class='w-full gap-2 flex flex-wrap justify-center m-10'>
         <div></div>
 
-        @forelse ($medias as $media)
+        @foreach($medias as $media)
+        @foreach($media->getMedia() as $mediaItem)        
         <div key={content} class="group relative rounded-lg overflow-hidden bg-white hover:shadow-2xl border-2">
-            @if ($media->type === 'image')
+            @if($mediaItem->type == 'image')
             <div class="p-4">
-                <img src="{{ asset('storage/media/images/' . $media->photo) }}" alt='City' class="h-72 w-full object-cover object-center">
+                <img src="{{ asset($mediaItem->getUrl()) }}" alt='' class="h-72 w-full object-cover object-center">
             </div>
-            @elseif ($media->type === 'video')
+            @elseif($mediaItem->type == 'video')
             <div class="p-4">
                 <video controls class="h-72 w-full object-cover object-center">
-                    <source src="{{ asset('storage/media/videos/' . $media->photo) }}" type="video/mp4">
+                    <source src="{{ asset($mediaItem->getUrl()) }}" type="video/mp4">
                 </video>
             </div>
             @endif
-            <p class="text-sm font-bold text-orange-500 text-center">{{ $media->photo }}</p>
+            <p class="text-sm font-bold text-orange-500 text-center">{{ $mediaItem->name }}</p>
 
             @auth
             @if(auth()->user()->role === 0)
             <div class="w-full">
-                <form action="{{ route('delete.media', $media->id) }}" method="post">
+                <form action="{{ route('delete.media', $mediaItem->id) }}" method="post">
                     @csrf
                     @method('DELETE')
                     <button type="submit" name="supprimer" class="w-full bg-black px-5 py-2 font-semibold text-gray-500 duration-75 hover:bg-gray-700 hover:text-purple-100">Supprimer</button>
@@ -60,9 +61,8 @@
             @endauth
          
         </div>
-        @empty
-        <p>No media Found</p>
-        @endforelse
+        @endforeach
+        @endforeach
         <div></div>
     </div>
 </div>
