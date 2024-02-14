bdcc8e17e54a1248b92bb152695a3caaa02169d1
Elgadisalma <elgadi.salma0@gmail.com>
2024-02-14 12:01:21 +0100

    Create newsLetter.blade.php

---
+++ resources/views/newsLetter.blade.php
@@ -0,0 +1,13 @@
+Hello valued subscribers,
+
+<h1>{{ $template->title }}</h1>
+
+<h3>{{ $template->description }}</h3>
+
+{{-- link --}}
+<a href="{{ $template->media }}"><h6>{{ $template->media }}</h6></a>
+
+Facebook: [Link]
+Twitter: [Link]
+LinkedIn: [Link]
+Instagram: [Link]
